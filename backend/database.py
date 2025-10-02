b4bb40ba973dbf4db002760cbe0c3a12965c98c9
oscar <oscgueviv1987@gmail.com>
2025-10-01 18:49:53 -0700

    code for calendar.py database.py

---
+++ backend/database.py
@@ -0,0 +1,7 @@
+# database.py
+room_database = [
+    {"name": "Single Room", "max_guests": 2, "features": ["1 Queen Bed", "Free WiFi", "Mini Fridge", "TV"], "quantity": 5, "price": 100},
+    {"name": "Double Room", "max_guests": 4, "features": ["2 Queen Beds", "Free WiFi", "Mini Fridge", "TV"], "quantity": 10, "price": 150},
+    {"name": "Family Room", "max_guests": 6, "features": ["2 Queen Beds", "Sofa-bed", "WiFi", "Crib Available"], "quantity": 6, "price": 200},
+    {"name": "VIP Suite", "max_guests": 3, "features": ["King Bed", "Living Area", "Balcony", "TV", "WiFi"], "quantity": 3, "price": 300},
+]
