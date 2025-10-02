50dc08d659c2eab32a9778163a70670733fdcd8e
MikeFelips99 <mfeldman2468@gmail.com>
2025-10-01 20:12:39 -0700

    Add files via upload

---
+++ backend/reservation class.py
@@ -0,0 +1,7 @@
+#Reservation Class.py
+
+class Reservation:
+    def __init__(self, room_type, check_in, check_out):
+        self.room_type = room_type
+        self.check_in = check_in
+        self.check_out = check_out
