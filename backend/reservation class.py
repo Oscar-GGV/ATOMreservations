3bd36b75a0a7d7587d3a3a8bab29d17a748efc0f
MikeFelips99 <mfeldman2468@gmail.com>
2025-11-21 21:54:20 -0800

    Add docstring to Reservation class
    Added docstring to Reservation class and its initializer.

--- backend/reservation class.py
+++ backend/reservation class.py
@@ -1,7 +1,21 @@
 #Reservation Class.py
 
 class Reservation:
+    """
+    Represents a reservation made by a customer.
+    Attributes:
+    - room_type: Type of room reserved
+    - check_in: Check-in date
+    - check_out: Check-out date
+    """
     def __init__(self, room_type, check_in, check_out):
+        """
+        Initializes a Reservation instance.
+        Args:
+        - room_type: Type of room reserved
+        - check_in: Check-in date
+        - check_out: Check-out date
+        """
         self.room_type = room_type
         self.check_in = check_in
         self.check_out = check_out
