3f353b21df3a0a16a26aa7c818b8d821768445a4
oscar <oscgueviv1987@gmail.com>
2025-12-03 23:50:48 -0800

    changed the search class, controler, method in reservation system, and calendar.py so that the search might be able to work with front end

--- backend/search.py
+++ backend/search.py
@@ -5,14 +5,12 @@ Programmer: Oscar Guevara
 date of code: November 5th, 2025"""
 class Search:
     """Represents a search query in the hotel reservation system."""
-    def __init__(self, check_in_date, check_out_date, room_type, guests):
+    def __init__(self, check_in_date, check_out_date, num_guests):
         """Initializes a Search object with the given parameters.
             parameters:
-            check_in_date (str): The check-in date for the search.
-            check_out_date (str): The check-out date for the search.
-            room_type (str): The type of room being searched for.
+            check_in_date (tuple): The check-in date for the search.
+            check_out_date (tuple): The check-out date for the search.
             guests (int): The number of guests for the search."""
         self.check_in_date = check_in_date
         self.check_out_date = check_out_date
-        self.room_type = room_type
-        self.guests = guests
+        self.num_guests = num_guests
